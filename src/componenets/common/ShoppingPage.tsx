7f5e73ccbff23dcc23d358c2089e78aac9cb47d3
emShalaby <medo.dice@gmail.com>
2024-09-15 12:55:06 +0300

    shopping page component and configuration for vercel build

--- src/componenets/common/ShoppingPage.tsx
+++ src/componenets/common/ShoppingPage.tsx
@@ -1,20 +1,34 @@
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import H1 from "./H1";
+import H3 from "./H3";
 
 export default function ShoppingPage() {
   const location = useLocation();
   const { state } = location;
   const { title, price, imageUrl, description } = state || {};
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div className="mt-10 flex w-full wide:justify-center">
-      <div className="flex">
-        <div className="flex-1">
-          <img src={imageUrl} alt="" />
+      <div className="flex gap-2">
+        <div className="w-[50%]  md:m-3 md:p-5">
+          <div
+            className={`max-h-[1000px] min-h-[300px] min-w-[300px] ${isLoading ? "animate-pulse" : ""}`}
+          >
+            <img
+              src={imageUrl}
+              alt={title}
+              className={`max-h-[1000px] min-h-[300px] min-w-[300px] transition-opacity duration-500 ${isLoading ? "opacity-0" : "opacity-100"}`}
+              onLoad={() => setIsLoading(false)}
+            />
+          </div>
         </div>
-        <div className="flex flex-1 flex-col">
-          <p>{title}</p>
+        <div className="m-2 flex w-[50%] flex-col gap-2 p-2 md:m-3 md:gap-5 md:p-5">
+          <H1>{title}</H1>
           <p>{description}</p>
-          <p>{price}</p>
+          <H3>{price}</H3>
+          <input type="number" className="border-2 border-solid" />
           <button className="bg-black text-white">Add to cart</button>
         </div>
       </div>
